9c1f9a2a57c03711eeeae5998b6099ffdd330fb0
liquan <liquan@syxysoft.com>
2020-01-07 14:56:15 +0800

    【新增】两个排序

---
+++ src/main/java/com/pgz/sort/MonkeySort.java
@@ -0,0 +1,85 @@
+package com.pgz.sort;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * 猴子排序
+ *
+ * @author liquan@syxysoft.com
+ * @date 2020-01-07
+ */
+public class MonkeySort {
+
+    private static int length = 8;
+    private static int[] array = new int[length];
+    private static int[] result = new int[length];
+
+    static {
+        generate();
+    }
+
+    /**
+     * 检查数组是否有序
+     *
+     * @return
+     */
+    private static boolean checkOrder(int array[]) {
+        for (int i = 1; i < length; i++) {
+            if (array[i] <= array[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void sort(int num, Random random) {
+        Set<Integer> numberSet = new HashSet<>();
+        System.out.println("第" + num + "次");
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(length);
+            while (numberSet.contains(index)) {
+                index = random.nextInt(length);
+            }
+            numberSet.add(index);
+            int number = array[index];
+            result[i] = number;
+            System.out.print(" " + result[i]);
+        }
+        System.out.println();
+    }
+
+    private static void generate() {
+        array[0] = 8;
+        array[1] = 5;
+        array[2] = 3;
+        array[3] = 6;
+        array[4] = 4;
+        array[5] = 9;
+        array[6] = 7;
+        array[7] = 10;
+//        array[8] = 1;
+//        array[9] = 2;
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int num = 0;
+        long startTime = System.currentTimeMillis();
+
+        while (true) {
+            sort(num++, random);
+            boolean isOrder = checkOrder(result);
+            if (isOrder) {
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("排序完成，耗时：" + (endTime - startTime) + " ms");
+
+    }
+
+
+}
---
+++ src/main/java/com/pgz/sort/SleepSort.java
@@ -0,0 +1,77 @@
+package com.pgz.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 睡眠排序
+ *
+ * @author liquan@syxysoft.com
+ * @date 2020-01-07
+ */
+public class SleepSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 7, 3, 8, 9, 2, 6, 5, 10};
+        List<Integer> result = sort(arr);
+        System.out.println(result.toString());
+    }
+
+    /**
+     * 睡眠排序
+     *
+     * @param arr
+     * @return
+     * @author liquan@syxysoft.com
+     * date 2020-01-07
+     **/
+    public static List<Integer> sort(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+        //创建指定长度的线程数组
+        SortThread[] sortThreads = new SortThread[arr.length];
+        CountDownLatch latch = new CountDownLatch(arr.length);
+        //指定每个线程数组的值
+        for (int i = 0; i < sortThreads.length; i++) {
+            sortThreads[i] = new SortThread(arr[i], result, latch);
+        }
+        //开启每个线程
+        for (SortThread sortThread : sortThreads) {
+            sortThread.start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
+
+
+class SortThread extends Thread {
+    int s;
+    List<Integer> result;
+    CountDownLatch latch;
+
+    public SortThread(int s, List<Integer> result, CountDownLatch latch) {
+        this.s = s;
+        this.result = result;
+        this.latch = latch;
+    }
+
+    public void run() {
+        try {
+            //睡眠指定的时间
+            sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //输出该数
+        result.add(s);
+        System.out.println(s);
+        latch.countDown();
+    }
+}
