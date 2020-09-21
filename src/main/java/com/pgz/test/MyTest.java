01688dba3c84d0d1f319fe94c2a49ab0f7a7fad8
liquan <liquan_pgz@qq.com>
2020-09-21 09:21:00 +0800

    【新增】测试类 Integer 内部存在缓存 -127~128

---
+++ src/main/java/com/pgz/test/MyTest.java
@@ -0,0 +1,28 @@
+package com.pgz.test;
+
+import org.junit.Test;
+
+/**
+ * //TODO 添加类/接口功能描述
+ *
+ * @author liquan_pgz@qq.com
+ * @date 2020-09-21
+ */
+public class MyTest {
+
+    /**
+     * Integer 内部存在缓存 -127~128
+     *
+     * @return
+     * @author liquan_pgz@qq.com
+     * date 2020-09-21
+     **/
+    @Test
+    public void testInteger() {
+        Integer a = 1000, b = 1000;
+        System.out.println(a == b);
+        Integer c = 100, d = 100;
+        System.out.println(c == d);
+    }
+
+}
