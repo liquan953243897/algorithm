b9b68d77f47fb1a3af88295940afcd72e8105b3f
liquan <liquan_pgz@qq.com>
2020-09-29 14:32:20 +0800

    【修改】交换代码位置，实现多客户端通信

--- src/main/java/com/pgz/net/tcp/TCPClient.java
+++ src/main/java/com/pgz/net/tcp/TCPClient.java
@@ -18,11 +18,12 @@ public class TCPClient {
         System.out.println("请输入一个英文字符串，服务器将返回其大写形式（输入exit退出）");
         while (true) {
             BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-            //创建客户端 Socket 并指明需要连接的服务器端的主机名及端口号
+            sentence = inFromUser.readLine();
+            //创建客户端 Socket 并指明需要连接的服务器端的主机名及端口号，即用即时创建连接
             Socket clientSocket = new Socket("localhost", 6789);
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            sentence = inFromUser.readLine();
+
             if (sentence.equals("exit")) break;
             //向服务器发送数据
             outToServer.writeBytes(sentence + '\n');
