d4fe159103028b926f73cc1188d923ed2023f19a
liquan <liquan_pgz@qq.com>
2020-10-13 14:19:50 +0800

    【添加】模拟的输入数据内容和格式 【添加】磁盘使用信息监控方法

--- src/main/java/com/pgz/test/MyTest.java
+++ src/main/java/com/pgz/test/MyTest.java
@@ -3,7 +3,19 @@ package com.pgz.test;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -63,13 +75,13 @@ public class MyTest {
         byte[] ba = {'%', 'w', 1, 3, 31};
         System.out.println(ba);
         for (byte b : ba) {
-            System.out.printf("%02x",b);//257701031f
+            System.out.printf("%02x", b);//257701031f
         }
     }
 
     @Test
     public void testHex2Byte() {
-        System.out.println((byte)Integer.parseInt("48",16));//31
+        System.out.println((byte) Integer.parseInt("52", 16));//31
     }
 
     @Test
@@ -93,9 +105,87 @@ public class MyTest {
     public void testByteToHex() {
         byte b = -86;
         String hex = Integer.toHexString(b & 0xFF);
-        if(hex.length() < 2){
+        if (hex.length() < 2) {
             hex = "0" + hex;
         }
         System.out.println(hex);
+    }
+
+    @Test
+    public void testWrite2File() {
+        byte[] data = {-86, -52, 2, 0, 2, 0, 9, 0, 1, 32, 16, 18, 20, 82, 0, 33, 18, 19, 0, 21};
+        File file = new File("D://out.doo");
+        try {
+            OutputStream os = new FileOutputStream(file);
+            InputStream is = new ByteArrayInputStream(data);
+            byte[] buff = new byte[1024];
+            int len = 0;
+            while ((len = is.read(buff)) != -1) {
+                os.write(buff, 0, len);
+            }
+            is.close();
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testWrite2FileMulti() {
+        byte[] data = {-86, -52, 2, 0, 2, 0, 9, 0, 1, 32, 16, 18, 20, 82, 0, 33, 18, 19, 0, 21};
+        File file = new File("D://out_multi.doo");
+        try {
+            OutputStream os = new FileOutputStream(file);
+            os.write(data);
+            os.write("\r\n".getBytes());
+            os.write(data);
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testReadFromFile() {
+        File file = new File("D://out.doo");
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+            ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
+
+            System.out.println("Available bytes:" + in.available());
+
+            byte[] temp = new byte[1024];
+            int size = 0;
+            while ((size = in.read(temp)) != -1) {
+                out.write(temp, 0, size);
+            }
+            in.close();
+
+            byte[] content = out.toByteArray();
+            System.out.println("Readed bytes count:" + content.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testReadMultiFromFile() {
+        File file = new File("D://out_multi.doo");
+        try {
+            FileReader in = new FileReader(file);
+            BufferedReader br = new BufferedReader(in);
+            ArrayList<byte[]> arr = new ArrayList<>();
+            String str;
+            while ((str = br.readLine()) != null) {
+                arr.add(str.getBytes());
+            }
+            in.close();
+            br.close();
+
+            System.out.println(arr.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
